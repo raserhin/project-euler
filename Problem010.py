7edf3fea6c205c3fa886298c0d769856b68e1e92
rafaelse <raserhin@gmail.com>
2019-12-09 13:27:14 +0100

    Optimization of Problem 10 x15 increase in perf

--- Problem010.py
+++ Problem010.py
@@ -8,16 +8,13 @@ from time import time
 
 
 def prime_generator(N=10):
-    yield 2
-    for i in range(3, N, 2):
-        is_prime = True
-        for divisor in range(3, ceil(sqrt(i)) + 1, 2):
-            if i % divisor == 0:
-                is_prime = False
-                break
-        if is_prime:
+    sieve = [False, False] + [True] * N # Added 0 and 1 
+    
+    for i in range(2, N+1):
+        if sieve[i]:
             yield i
-
+            for j in range(i*i, N, i):
+                sieve[j] = False
 
 start_time = time()
 result = sum(prime_generator(N=2_000_000))
