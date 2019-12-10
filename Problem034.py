f39c696a140ab87bf33a263104253dd94c6cc46a
rafaelse <raserhin@gmail.com>
2019-12-10 14:26:22 +0100

    Adding Problem 34

---
+++ Problem034.py
@@ -0,0 +1,20 @@
+"""
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+"""
+from time import time
+from math import factorial
+
+def is_curious(x):
+    return sum(factorial(int(i)) for i in str(x)) == x
+
+
+start_time = time()
+result = [i for i in range(3, 50_000) if is_curious(i) ]
+final_time = time()
+
+print(result)
+print(f"The result {sum(result)} has been found in {final_time - start_time:.6f}s")
