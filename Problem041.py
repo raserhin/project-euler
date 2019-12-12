2dd577f74a3d92562fdacf95d7549e2e3bf7c135
rafaelse <raserhin@gmail.com>
2019-12-12 15:49:04 +0100

    Problem 41

---
+++ Problem041.py
@@ -0,0 +1,45 @@
+"""
+Project Euler Problem 41
+========================
+
+We shall say that an n-digit number is pandigital if it makes use of all
+the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital
+and is also prime.
+
+What is the largest n-digit pandigital prime that exists?
+"""
+from itertools import permutations
+from math import sqrt, floor
+
+
+def is_prime(x, lookup_table):
+    """ Determine if x is prime"""
+    for prime in lookup_table:
+        if x % prime ==0:
+            return False
+    return True
+def prime_generator(N=10):
+    sieve = [False, False] + [True] * N  # Added 0 and 1
+
+    for i in range(2, len(sieve)):
+        if sieve[i]:
+            for j in range(i*i, len(sieve), i):
+                sieve[j] = False
+    return [i for i, is_prime in enumerate(sieve) if is_prime]
+
+
+n = 9
+lookup_prime = prime_generator(floor(sqrt(10**(n-1)) + 1))
+
+
+
+while n > 4:
+    digits = list(range(1, n+1))[::-1]
+
+    for perm in permutations(digits, n):
+        num = int("".join([str(i) for i in perm]))
+        if is_prime(num, lookup_prime):
+            print(num)
+            n = 0
+            break
+    n -= 1
