0b87b11409e25f4c9b83e82870b97913213a4a1c
rafaelse <raserhin@gmail.com>
2019-12-12 13:29:59 +0100

    Problem 40

--- Problem040.py
+++ Problem040.py
@@ -25,8 +25,11 @@ def digit_generator(N=1_000_000):
     while N > i:
         for _i in str(number):
             yield _i
-            i+=1
-        number+=1
+            i += 1
+        number += 1
+
+
+
 
 def get_digit(n):
     """ Get the digit in nth decimal place"""
@@ -37,39 +40,29 @@ def get_digit(n):
     """
     i = 0
     start = 0
-    while start < n: 
-        start += (i+1)*9*(10**i)
-        i += 1
-    
+    done = False
+    while not done:
+        step = (i+1)*9*(10**i)
+        if start + step > n:
+            done = True
+        else:
+            start += step
+            i += 1
 
     num_digits = i + 1
-    print(start, num_digits)
 
-    offset =  n -  start
-    number = offset // num_digits + 10**(num_digits-1) - 1
-    _i = (offset + 1) % num_digits
-    print(n, start, offset, number, _i)
-    print(str(number)[_i])
-
-
-
-get_digit(11)
-get_digit(12)
-get_digit(13)
-get_digit(14)
-get_digit(15)
-get_digit(16)
-get_digit(100)
+    offset = n - start
+    number = offset // num_digits + \
+        (10**(num_digits-1) if (num_digits - 1) else 0)
+    _i = (offset - 1) % num_digits
+    print(number)
+    return int(str(number)[_i])
 
 
-# for i in range(0, 7):
-#     get_digit(10**i)
 
 
-# result = 1
-# for i, value in enumerate(digit_generator(N=1_000_000)):
-#     a= log10(i+1)
-#     if a == floor(a):
-#         result *= int(value)
-# print(result)
+result = 1
 
+for i in range(0, 7):
+    result *= get_digit(10**i)
+print(result)
